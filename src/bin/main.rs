1786d4ce80cbdef87441c06f976936887f4ffa97
Guillaume <guillaume.dadoy@ik.me>
2022-01-25 23:23:33 +0100

    Oopsie

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -8,10 +8,10 @@ use hello::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        let pool = ThreadPool::new(4);
 
         pool.execute(|| {
             handle_connection(stream);
